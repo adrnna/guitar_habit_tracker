b959128480cbb6260d444d7785bd0a22510930c5
adrnna <ad.porebska@gmail.com>
2024-01-05 16:26:33 +0100

    feat: display exercises for each routine

--- frontend/src/components/RoutineStripe.js
+++ frontend/src/components/RoutineStripe.js
@@ -1,27 +1,45 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import textContent from '../../textContent';
 
 
 const RoutineStripe = ({index, routine, exerciseList}) => {  
-  
-    console.log(routine.exercises);
+
+    const exerciseIds = routine.exercises;
+
+    const filteredExercises = exerciseIds.map((exerciseId) => {
+        return exerciseList.find((exercise) => exercise.id === exerciseId);
+    });
 
     return (
         <div key={index} className="stripe-container">
             <div className="stripe-and-collapsible">
                 <div className="overlay-stripe routine" >
-                    <p className="routine_title">Routine Name: {routine.routine_name}</p>
-                    {/* Add more content related to each routine */}
+                    <p className="routine_title">{textContent.routineTitle}{routine.routine_name}</p>
                     <div className="routine-table-container">
                     <table className="routine-table">
                         <thead>
                             <tr>
-                                <th>Exercise Name</th>
-                                <th>Time (min)</th>
-                                <th>Description</th>
-                                <th>Link</th>
+                                <th>{textContent.type}</th>
+                                <th>{textContent.exName}</th>
+                                <th>{textContent.time}</th>
+                                <th>{textContent.description}</th>
+                                <th>{textContent.link}</th>
                             </tr>
                         </thead>
+                        <tbody>
+                            {filteredExercises.map((exercise) => (
+                            // check if exercise is defined before trying to access its properties
+                            exercise ? (
+                                <tr key={exercise.id}>
+                                  <td>{exercise.exercise_type}</td>
+                                  <td>{exercise.exercise_name}</td>
+                                  <td>{exercise.time}</td>
+                                  <td>{exercise.description}</td>
+                                  <td>{exercise.link}</td>
+                                </tr>
+                              ) : null
+                            ))}
+                        </tbody>
                     </table>
                     </div>
                 </div>
